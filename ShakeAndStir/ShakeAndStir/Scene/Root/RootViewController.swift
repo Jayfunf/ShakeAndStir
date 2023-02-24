4b2e86fa73a4f5ee34c96bafaafa27b08ee3eb22
ChoMinhyun <cho.minhyun@cashwalk.io>
2023-02-24 17:11:36 +0900

    init

---
+++ ShakeAndStir/ShakeAndStir/Scene/Root/RootViewController.swift
@@ -0,0 +1,26 @@
+//
+//  RootViewController.swift
+//  ShakeAndStir
+//
+//  Created by Minhyun Cho on 2023/02/24.
+//
+
+import UIKit
+
+import ReactorKit
+import RxSwift
+
+class RootViewController: UIViewController {
+    
+    var disposeBag = DisposeBag()
+    
+    func bind(reactor: RootViewReactor) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+    }
+}
---
+++ ShakeAndStir/ShakeAndStir/Scene/Root/RootViewReactor.swift
@@ -0,0 +1,17 @@
+//
+//  RootViewReactor.swift
+//  ShakeAndStir
+//
+//  Created by Minhyun Cho on 2023/02/24.
+//
+
+import Foundation
+import ReactorKit
+
+//class RootViewReactor: Reactor {
+//    typealias Action = <#type#>
+//    
+//    typealias State = <#type#>
+//    
+//    
+//}

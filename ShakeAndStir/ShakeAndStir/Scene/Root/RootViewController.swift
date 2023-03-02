a3a6df41b856e607439320744cc91b614951529b
ChoMinhyun <cho.minhyun@cashwalk.io>
2023-03-02 17:20:22 +0900

    Update Sample Code 추가

--- ShakeAndStir/ShakeAndStir/Scene/Root/RootViewController.swift
+++ ShakeAndStir/ShakeAndStir/Scene/Root/RootViewController.swift
@@ -8,19 +8,99 @@
 import UIKit
 
 import ReactorKit
+import RxCocoa
 import RxSwift
+import SnapKit
+import Then
 
-class RootViewController: UIViewController {
+final class RootViewController: UIViewController, View {
     
+    typealias Reactor = RootViewReactor
     var disposeBag = DisposeBag()
     
-//    func bind(reactor: RootViewReactor) {
-//        
-//    }
+//MARK: - UI Components
+    var loginButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Gettin Start!", for: .normal)
+        button.tintColor = .white
+        return button
+    }()
+    
+    var managerButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Go To ManagerMode", for: .normal)
+        button.tintColor = .white
+        return button
+    }()
+    
+    var indicator: UIActivityIndicatorView = {
+        let view = UIActivityIndicatorView()
+        return view
+    }()
+    
+    var testLabel: UILabel = {
+        let label = UILabel()
+        label.text = "테스트"
+        label.textColor = .white
+        return label
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .blue
+        view.backgroundColor = .black
+        setupView()
+        self.reactor = RootViewReactor()
+    }
+
+    func bind(reactor: RootViewReactor) {
+        loginButton.rx.tap
+            .map { Reactor.Action.clickToStart }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+        managerButton.rx.tap
+            .map { Reactor.Action.clickToManage }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+        reactor.state
+            .map { $0.isLoading }
+            .distinctUntilChanged()
+            .bind(to: indicator.rx.isAnimating)
+            .disposed(by: disposeBag)
+        
+        reactor.state
+            .map { String($0.isLoading)}
+            .distinctUntilChanged()
+            .bind(to: testLabel.rx.text)
+            .disposed(by: disposeBag)
+    }
+}
+
+extension RootViewController {
+    func setupView() {
+        view.addSubview(loginButton)
+        loginButton.snp.makeConstraints {
+            $0.centerX.centerY.equalToSuperview()
+        }
+        
+        view.addSubview(managerButton)
+        managerButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalTo(loginButton.snp.centerY).offset(40)
+        }
+        
+        view.addSubview(testLabel)
+        testLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalTo(loginButton.snp.centerY).offset(-50)
+        }
+        
+        view.addSubview(indicator)
+        indicator.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalTo(testLabel.snp.top).offset(-20)
+        }
     }
 }
--- ShakeAndStir/ShakeAndStir/Scene/Root/RootViewReactor.swift
+++ ShakeAndStir/ShakeAndStir/Scene/Root/RootViewReactor.swift
@@ -5,13 +5,65 @@
 //  Created by Minhyun Cho on 2023/02/24.
 //
 
-import Foundation
+import RxSwift
 import ReactorKit
 
-//class RootViewReactor: Reactor {
-//    typealias Action = <#type#>
-//    
-//    typealias State = <#type#>
-//    
-//    
-//}
+class RootViewReactor: Reactor {
+    enum Action {
+        case clickToManage
+        case clickToStart
+    }
+    
+    enum Mutation {
+        case openMainView
+        case openManagerView
+        case setLoading(Bool)
+    }
+    
+    struct State {
+        var isLoading: Bool = false
+        var isManagerMode: Bool = false
+        var isLoginSuccess: Bool = false
+    }
+
+    let initialState: State
+    
+    init() {
+        self.initialState = State(isLoading: false, isManagerMode: false, isLoginSuccess: false)
+    }
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        print("CMH :: Mutate")
+        switch action {
+        case .clickToStart:
+            return Observable.concat([
+                Observable.just(Mutation.setLoading(true)),
+                Observable.just(Mutation.openMainView)
+                    .delay(RxTimeInterval.seconds(1), scheduler: MainScheduler.instance),
+                Observable.just(Mutation.setLoading(false))
+            ])
+        case .clickToManage:
+            return Observable.concat([
+                Observable.just(Mutation.setLoading(true)),
+                Observable.just(Mutation.openManagerView)
+                    .delay(RxTimeInterval.seconds(1), scheduler: MainScheduler.instance),
+                Observable.just(Mutation.setLoading(false))
+            ])
+        }
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        print("CMH :: Reduce")
+        var newState = state
+        switch mutation {
+        case let .setLoading(isLoading):
+            newState.isLoading = isLoading
+        case .openManagerView:
+            newState.isManagerMode = true
+        case .openMainView:
+            newState.isLoginSuccess = true
+        }
+        
+        return newState
+    }
+}

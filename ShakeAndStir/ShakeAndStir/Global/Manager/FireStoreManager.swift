1da5608f301bcd8bc02ef25cc3e913fad011d622
Jayfunf <simh3077@gmail.com>
2023-03-07 15:54:33 +0900

    Add UserModel, FireStoreManager 추가

---
+++ ShakeAndStir/ShakeAndStir/Global/Manager/FireStoreManager.swift
@@ -0,0 +1,13 @@
+//
+//  FireStoreManager.swift
+//  ShakeAndStir
+//
+//  Created by Minhyun Cho on 2023/03/07.
+//
+
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+
+class FireStoreManager {
+    
+}
---
+++ ShakeAndStir/ShakeAndStir/Global/Model/UserModel.swift
@@ -0,0 +1,12 @@
+//
+//  UserModel.swift
+//  ShakeAndStir
+//
+//  Created by Minhyun Cho on 2023/03/07.
+//
+
+import Foundation
+
+struct UserModel: Codable {
+   
+}

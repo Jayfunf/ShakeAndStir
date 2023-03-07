005b38f6c851d56755d5bb3cac2316c6256f775e
Jayfunf <simh3077@gmail.com>
2023-03-07 17:27:01 +0900

    Update 모델 추가

--- ShakeAndStir/ShakeAndStir/Global/Model/CocktailModel.swift
+++ ShakeAndStir/ShakeAndStir/Global/Model/CocktailModel.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+struct CocktailModel: Codable {
+    let name: String
+    let base: [String]
+    let taste: [String]
+    let price: String
+    
+    init() {
+        self.name = ""
+        self.base = [""]
+        self.taste = [""]
+        self.price = ""
+    }
+    
+    init(name: String, base: [String], taste: [String], price: String) {
+        self.name = name
+        self.base = base
+        self.taste = taste
+        self.price = price
+    }
+    
+}

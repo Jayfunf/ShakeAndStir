20bbec112fc905f86b6bd234089896ccaf76ff90
Jayfunf <simh3077@gmail.com>
2023-03-07 17:27:49 +0900

    Update FireStoreManager 기능 추가 - 유저 데이터 저장, 유저 데이터 불러오기, 칵테일 불러오기

--- ShakeAndStir/ShakeAndStir/Global/Manager/FireStoreManager.swift
+++ ShakeAndStir/ShakeAndStir/Global/Manager/FireStoreManager.swift
@@ -8,6 +8,41 @@
 import FirebaseFirestore
 import FirebaseFirestoreSwift
 
-class FireStoreManager {
+final class FireStoreManager {
+    private var documentListner: ListenerRegistration?
+    static var shared = FireStoreManager()
     
+    enum CollectionPaths: String {
+        case Users, Cocktails
+    }
+    
+    let document = Firestore.firestore()
+    
+    func setUserData(_ model: UserModel) throws {
+        let userName: String = model.name
+        print(CollectionPaths.Users)
+        let collectionPath = "\(CollectionPaths.Users)"
+        
+        try document.collection(collectionPath).document(userName).setData(from: model)
+    }
+    
+    func getUserData() async throws -> [UserModel] {
+        let collectionPath = "\(CollectionPaths.Users)"
+        let result = try await document.collection(collectionPath).getDocuments()
+        let data = try result.documents.map { snapshot in
+            try snapshot.data(as: UserModel.self)
+        }
+        
+        return data
+    }
+    
+    func loadCockTailList() async throws -> [CocktailModel] {
+        let collectionPath = "\(CollectionPaths.Cocktails)"
+        let result = try await document.collection(collectionPath).order(by: "name", descending: true).getDocuments()
+        let data = try result.documents.map { snapshot in
+            try snapshot.data(as: CocktailModel.self)
+        }
+        
+        return data
+    }
 }

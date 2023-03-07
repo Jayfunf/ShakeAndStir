027abfbab7fc5bc12ea56d50b22e156ba6b3ac71
Jayfunf <simh3077@gmail.com>
2023-03-07 15:55:01 +0900

    Update UserModel 구현

--- ShakeAndStir/ShakeAndStir/Global/Model/UserModel.swift
+++ ShakeAndStir/ShakeAndStir/Global/Model/UserModel.swift
@@ -8,5 +8,27 @@
 import Foundation
 
 struct UserModel: Codable {
-   
+    let name: String
+    let preferFlavor: [String]
+    
+    init() {
+        self.name = ""
+        self.preferFlavor = [""]
+    }
+    
+    init(name: String, preferFlavor: [String]) {
+        self.name = name
+        self.preferFlavor = preferFlavor
+    }
+    
+    private enum CodingKeys: String, CodingKey {
+        case name
+        case preferFlavor
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        name = try values.decode(String.self, forKey: .name)
+        preferFlavor = try values.decode([String].self, forKey: .preferFlavor)
+    }
 }

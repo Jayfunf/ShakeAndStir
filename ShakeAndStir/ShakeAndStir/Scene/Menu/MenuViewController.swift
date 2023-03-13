7a390be096df620e92153fc4909373edeb332f15
Jayfunf <simh3077@gmail.com>
2023-03-13 17:18:44 +0900

    Update TableView추가 작업 진행

--- ShakeAndStir/ShakeAndStir/Scene/Menu/MenuViewController.swift
+++ ShakeAndStir/ShakeAndStir/Scene/Menu/MenuViewController.swift
@@ -12,18 +12,22 @@ import RxCocoa
 import RxSwift
 import SnapKit
 
-final class MenuViewController: UIViewController, View {
-    
+final class MenuViewController: UIViewController, View, UITableViewDataSource, UITableViewDelegate {
     
     typealias Reactor = MenuViewReactor
     var disposeBag = DisposeBag()
     
     var isManagerMode: Bool = false
     
-//MARK: - UI Components
+    var tableView: UITableView!
+    
+    var cocktailModel: [CocktailModel] = []
+    var cocktailHeaders: [String] = []
+    
+    //MARK: - UI Components
     var makeLabel: UILabel = {
         let label = UILabel()
-        label.text = "Test Label"
+        label.text = ""
         label.textColor = .white
         return label
     }()
@@ -43,6 +47,30 @@ final class MenuViewController: UIViewController, View {
         self.reactor = MenuViewReactor()
         
         print("CMh :: isManagerMode -", isManagerMode)
+        Task {
+            do {
+                cocktailModel = try await FireStoreManager.shared.loadCockTailList()
+                print("CockTailModel", cocktailModel)
+                tableView.reloadData()
+            } catch {
+                print("Cocktail Error")
+            }
+        }
+        
+        // Set up table view
+        tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints {
+            $0.top.equalTo(button.snp.bottom).offset(20)
+            $0.left.right.bottom.equalToSuperview()
+        }
+    }
+    
+    deinit {
+        print("Deinit - <MenuViewController>")
     }
     
     func bind(reactor: MenuViewReactor) {
@@ -68,5 +96,41 @@ final class MenuViewController: UIViewController, View {
     
     @objc func testbutton() {
         dismiss(animated: true, completion: nil) // completion으로 로티 실행
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cocktailModel.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let cocktail = cocktailModel[indexPath.row]
+        cocktail.base.forEach {
+            if $0 == cocktailHeaders[indexPath.section] {
+                cell.textLabel?.text = cocktail.name
+            }
+        }
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        var sections: [String] = []
+        cocktailModel.forEach { model in
+            model.base.forEach {
+                sections.append($0)
+            }
+        }
+        
+        let setSections = Set(sections)
+        let uniquSections = Array(setSections)
+        
+        cocktailHeaders = uniquSections.sorted()
+        print("UnitSections", cocktailHeaders)
+        
+        return uniquSections.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return cocktailHeaders[section]
     }
 }

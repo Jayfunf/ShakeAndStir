92e5c766bef1f50fa661720071c65781d3adc14e
Minhyun Cho <simh3077@gmail.com>
2023-03-17 15:54:42 +0900

    Update 테스트 코드 추가

--- ShakeAndStir/ShakeAndStir/Scene/Menu/MenuDetailViewController.swift
+++ ShakeAndStir/ShakeAndStir/Scene/Menu/MenuDetailViewController.swift
@@ -5,4 +5,21 @@
 //  Created by Minhyun Cho on 2023/03/13.
 //
 
-import Foundation
+import UIKit
+
+class MenuDetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .black
+        
+        let backButton = UIBarButtonItem(title: "안할래요", style: .plain, target: self, action: #selector(backToMenu))
+        navigationItem.leftBarButtonItem = backButton
+    }
+    
+    @objc private func backToMenu() {
+        dismiss(animated: true)
+    }
+    
+}
